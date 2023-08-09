ba3319294c4b8ff1490f985b49f56b1a233cd5f4
tapeshnagarwal <tapeshnagarwal@gmail.com>
2023-08-09 18:58:36 -0400

    Prototype 1; contains functional scenario to validate search result from Google are related

--- src/test/java/StepDefinitions/googleStepDef.java
+++ src/test/java/StepDefinitions/googleStepDef.java
@@ -7,13 +7,18 @@ import org.apache.logging.log4j.LogManager;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class googleStepDef {
@@ -23,17 +28,14 @@ public class googleStepDef {
 
     @Given("initialized browser lands on {string}")
     public void initialized_browser_lands_on(String url) {
-        // Write code here that turns the phrase above into concrete actions
-
-//        WebDriver driver = null;
         System.setProperty("webdriver.chrome.driver", "/Users/tapeshnagarwal/Documents/Projects/Spiral/drivers/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
         driver.navigate().to(url);
-        logger.info("Current URL: "+ driver.getCurrentUrl());
-//        throw new io.cucumber.java.PendingException();
+        logger.info("Current URL: " + driver.getCurrentUrl());
     }
+
     @When("automation inputs {string} into the search field")
     public void automation_inputs_into_the_search_field(String string) {
         // Write code here that turns the phrase above into concrete actions
@@ -42,19 +44,26 @@ public class googleStepDef {
         searchBar.sendKeys(Keys.RETURN);
         logger.info("Test");
     }
+
     @Then("automation validates results are related to {string}")
     public void automation_validates_results_are_related_to(String string) {
-        // Write code here that turns the phrase above into concrete actions
-
-//    XPATH FOR SEARCH RESULTS:    //*[@id="rso"]/div/div
-        logger.info("Test");
-//        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(driver.getTitle().contains(string));
+        List<WebElement> resultLinks;
+        List<String> validResultLinks = new ArrayList<>();
+        resultLinks = driver.findElements(By.xpath("//*[@id='rso']/div[@class='MjjYud']//a"));
+        for (WebElement link : resultLinks) {
+            if (link.getText().contains(string))
+                validResultLinks.add(link.getText());
+        }
+        logger.info("Valid Results related to " + string + "are as following: ");
+        for (String result : validResultLinks) {
+            logger.info(result);
+        }
     }
 
     @And("browser shuts down")
     public void browser_shuts_down() {
         driver.close();
     }
-
 
 }
--- src/test/resources/Features/google.feature
+++ src/test/resources/Features/google.feature
@@ -3,7 +3,7 @@ Feature: Google Search Feature
   Background:
     Given initialized browser lands on 'https://google.com'
 
-    Scenario: Google Search 'Ducks'
-      When automation inputs 'Ducks' into the search field
-      Then automation validates results are related to 'Ducks'
-      And browser shuts down
+  Scenario: Google Search 'Ducks'
+    When automation inputs 'Ducks' into the search field
+    Then automation validates results are related to 'Ducks'
+    And browser shuts down

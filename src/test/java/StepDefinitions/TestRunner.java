72df69c83f88ab040dae96cf92d781afa83dd60a
tapeshnagarwal <tapeshnagarwal@gmail.com>
2023-08-09 15:16:13 -0400

    Implement Reporter configs

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features/google.feature", glue={"StepDefinitions"})
+@CucumberOptions(features="src/test/resources/Features/google.feature",
+        glue={"StepDefinitions"},
+        plugin = {"pretty", "html:target/Reports/HtmlReports.html"})
 public class TestRunner {
 
 }
--- src/test/java/StepDefinitions/googleStepDef.java
+++ src/test/java/StepDefinitions/googleStepDef.java
@@ -2,6 +2,7 @@ package StepDefinitions;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
@@ -18,7 +19,27 @@ public class googleStepDef {
     @When("browser directs to {string}")
     public void browser_directs_to(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        logger.info("Test");
+//        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("initialized browser lands on {string}")
+    public void initialized_browser_lands_on(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        logger.info("Test");
+//        throw new io.cucumber.java.PendingException();
+    }
+    @When("automation inputs {string} into the search field")
+    public void automation_inputs_into_the_search_field(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        logger.info("Test");
+//        throw new io.cucumber.java.PendingException();
+    }
+    @Then("automation validates results are related to {string}")
+    public void automation_validates_results_are_related_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        logger.info("Test");
+//        throw new io.cucumber.java.PendingException();
     }
 
 
